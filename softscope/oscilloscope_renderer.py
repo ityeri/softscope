0425e15e65a8f657726c3badcd3a559a6dd1964d
it <yeongbin0909@naver.com>
2025-02-03 00:59:21 +0900

    set_surface 메서드 추가, 임시 스텝기능 추가

--- softscope/oscilloscope_renderer.py
+++ softscope/oscilloscope_renderer.py
@@ -5,13 +5,37 @@ from numpy.typing import NDArray
 from softscope.typing import AudioData, SingleSample
 
 class OscilloscopeRenderer:
-    def __init__(self, surface: pygame.Surface, surface_center: tuple[int, int], surface_radius: int):
+    def __init__(self, surface: pygame.Surface, *,
+                 surface_center: tuple[int, int] | None = None,
+                 surface_radius: int | None = None):
+
         self.surface: pygame.Surface = surface # 알파채널 Surface
         self.buffer: NDArray[NDArray[numpy.float64]] = np.empty(shape=(0, 2))
 
-        self.surface_center_x: int = surface_center[0]
-        self.surface_center_y: int = surface_center[1]
-        self.surface_radius: int = surface_radius
+        self.surface_center_x: int = None
+        self.surface_center_y: int = None
+        self.surface_radius: int = None
+
+        self.set_surface(surface,
+                         surface_center=surface_center,
+                         surface_radius=surface_radius)
+
+
+    def set_surface(self, surface: pygame.Surface, *,
+                    surface_center: tuple[int, int] | None = None,
+                    surface_radius: int | None = None):
+        self.surface = surface
+
+        if surface_center is None:
+            self.surface_center_x = self.surface.get_width() / 2
+            self.surface_center_y = self.surface.get_height() / 2
+        else: self.surface_center_x, self.surface_center_y = surface_center
+
+        if surface_radius is None:
+            if self.surface.get_width() < self.surface.get_height():
+                self.surface_radius = self.surface.get_width() / 2
+            else: self.surface_radius = self.surface.get_height() / 2
+        else: self.surface_radius = surface_radius
 
 
 
@@ -19,15 +43,19 @@ class OscilloscopeRenderer:
         self.buffer = numpy.concatenate((self.buffer, data))
 
     def render(self):
-        self.surface.fill((0, 0, 0, 0))
+        cover_surface = pygame.Surface(self.surface.get_size(), pygame.SRCALPHA)
+        cover_surface.fill((0, 0, 0, 60))
+        self.surface.blit(cover_surface, (0, 0))
 
+        step = 10
 
+        for i in range(1, self.buffer.shape[0] // step):
+            sample_index = i*step
 
-        for i, sample_index in enumerate(self.buffer[1:]):
-            before_sample = self.buffer[i - 1]
-            current_sample = self.buffer[i]
+            before_sample = self.buffer[sample_index - step]
+            current_sample = self.buffer[sample_index]
 
-            pygame.draw.line(self.surface, (255, 0, 0, 30),
+            pygame.draw.line(self.surface, (255, 0, 0, 127),
                              self.sample_to_surface_value(before_sample),
                              self.sample_to_surface_value(current_sample))
 
