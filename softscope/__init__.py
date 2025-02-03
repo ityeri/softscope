deac9e341c140eb37ac8de12f4278c00c6922b30
ityeri <yeongbin0909@naver.com>
2025-02-03 14:02:10 +0900

    oscilloscope_style 추가

--- softscope/__init__.py
+++ softscope/__init__.py
@@ -3,9 +3,15 @@ from softscope.live_audio_file_manager import LiveAudioFileManager
 
 from softscope.typing import AudioData
 
+from softscope.oscilloscope_style import OscilloscopeStyle, OscilloscopeType
+
+
 __all__ = [
     "OscilloscopeRenderer",
     "LiveAudioFileManager",
 
-    "AudioData"
+    "AudioData",
+
+    "OscilloscopeStyle",
+    "OscilloscopeType"
 ]
