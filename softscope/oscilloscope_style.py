3eb75e0b9f4fb878d7f0bbdc5ae6b219dd447562
ityeri <yeongbin0909@naver.com>
2025-02-03 14:02:21 +0900

    oscilloscope_style 추가

--- softscope/oscilloscope_style.py
+++ softscope/oscilloscope_style.py
@@ -1,3 +1,18 @@
+import dataclasses
+from enum import Enum
+
+
+class OscilloscopeType(Enum):
+    BASIC = 0
+    BASIC_LIGHTING = 1
+    BLUR_LIGHTING = 2
+
+
+@dataclasses.dataclass
 class OscilloscopeStyle:
-    def __init__(self):
-        ... # TODO
+    type: OscilloscopeType
+    color: tuple[int, int, int, int]
+    step: float = 1
+
+    blur_range: float | None = None
+    blur_focus_level: float | None = None
