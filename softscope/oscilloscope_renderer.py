9a5e928e2eeac5134036b09d889abc22ca9fd57a
it <yeongbin0909@naver.com>
2025-02-03 23:39:27 +0900

    그래프 graph_step 속성 추가

--- softscope/oscilloscope_renderer.py
+++ softscope/oscilloscope_renderer.py
@@ -10,32 +10,41 @@ class OscilloscopeRenderer:
     def __init__(self, surface: pygame.Surface, *,
                  surface_center: tuple[int, int] | None = None,
                  surface_radius: int | None = None,
-                 scope_style: OscilloscopeStyle | None = None):
+                 graph_amplify: float = 1,
+
+                 scope_style: OscilloscopeStyle | None = None,
+                 graph_step: float = 1):
 
         self.surface: pygame.Surface = surface # 알파채널 Surface
         self.buffer: NDArray[NDArray[numpy.float64]] = np.empty(shape=(0, 2))
 
         self.surface_center_x: int = None
         self.surface_center_y: int = None
+
         self.surface_radius: int = None
+        self.graph_amplify: float = None
+        self.graph_step: float = graph_step
 
         self.set_surface(surface,
                          surface_center=surface_center,
-                         surface_radius=surface_radius)
+                         surface_radius=surface_radius,
+                         graph_amplify=graph_amplify)
 
         self.scope_style: OscilloscopeStyle
 
         if scope_style is None:
             self.scope_style = OscilloscopeStyle(
                 type= OscilloscopeType.BASIC,
-                color= (255, 255, 255, 200)
+                color= (255, 255, 255, 127)
             )
         else: self.scope_style = scope_style
 
 
     def set_surface(self, surface: pygame.Surface, *,
                     surface_center: tuple[int, int] | None = None,
-                    surface_radius: int | None = None):
+                    surface_radius: int | None = None,
+                    graph_amplify: float = 1):
+
         self.surface = surface
 
         if surface_center is None:
@@ -45,9 +54,11 @@ class OscilloscopeRenderer:
 
         if surface_radius is None:
             if self.surface.get_width() < self.surface.get_height():
-                self.surface_radius = self.surface.get_width() / 2 * 2
-            else: self.surface_radius = self.surface.get_height() / 2 * 2
+                self.surface_radius = self.surface.get_width() / 2
+            else: self.surface_radius = self.surface.get_height() / 2
         else: self.surface_radius = surface_radius
+
+        self.graph_amplify = graph_amplify
 
 
 
@@ -62,14 +73,14 @@ class OscilloscopeRenderer:
         before_sample_index: int = 0
         current_sample_index: int = 0
 
-        for i in range(1, int(self.buffer.shape[0] / self.scope_style.step)):
+        for i in range(1, int(self.buffer.shape[0] / self.graph_step)):
             before_sample_index = current_sample_index
-            current_sample_index = int(i*self.scope_style.step)
+            current_sample_index = int(i*self.graph_step)
 
             before_sample = self.buffer[before_sample_index]
             current_sample = self.buffer[current_sample_index]
 
-            pygame.draw.line(self.surface, (255, 255, 255, 1),
+            pygame.draw.line(self.surface, self.scope_style.color,
                              self.sample_to_surface_value(before_sample),
                              self.sample_to_surface_value(current_sample))
 
@@ -81,6 +92,6 @@ class OscilloscopeRenderer:
 
 
     def sample_to_surface_value(self, sample: SingleSample) -> tuple[float, float]:
-        x = float(sample[0] * self.surface_radius + self.surface_center_x)
-        y = float(-sample[1] * self.surface_radius + self.surface_center_y)
+        x = float(sample[0] * self.surface_radius * self.graph_amplify  +  self.surface_center_x)
+        y = float(-sample[1] * self.surface_radius * self.graph_amplify  +  self.surface_center_y)
         return x, y
